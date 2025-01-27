4917ecced711b586fde73a575bcd33eab2f5b923
perigmes <clemchallans@gmail.com>
2025-01-27 10:40:17 +0100

    modif uploads

--- server/upload.mjs
+++ server/upload.mjs
@@ -2,7 +2,7 @@ import multer from "multer";
 // Configuration de multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./uploads/"); // Dossier temporaire pour stocker les fichiers
+    cb(null, "./pictures"); // Dossier temporaire pour stocker les fichiers
   },
   filename: (req, file, cb) => {
     console.log('test'+file.filename)
