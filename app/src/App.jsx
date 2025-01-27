798bf2a0056b8c1826e7bdb84f2b388d555a907a
Pierrickbrd <Pierrick.Breaud.Etu@univ-lemans.fr>
2025-01-27 17:49:43 +0100

    Création du header de l'application

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,15 +1,25 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import ListObjects from './pages/ListObjects';
-import MainHeader from './components/MainHeader';
-import { Formulaire } from './pages/Formulaire';
-import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
-import './assets/styles/commun.scss';
-import './assets/styles/card.scss';
-import { loadMateriel, loadReservation } from './features/demande/reservationsAsyncAction';
-import { Reservation } from './pages/Reservation';
-import Demarches from './pages/Demarches';
-import NavFormBtns from './components/objects/NavFormBtns';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import ListObjects from "./pages/ListObjects";
+import MainHeader from "./components/MainHeader";
+import { Formulaire } from "./pages/Formulaire";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+import "./assets/styles/commun.scss";
+import "./assets/styles/card.scss";
+import {
+  loadMateriel,
+  loadReservation,
+} from "./features/demande/reservationsAsyncAction";
+import { Reservation } from "./pages/Reservation";
+import Demarches from "./pages/Demarches";
+import NavFormBtns from "./components/objects/NavFormBtns";
+import Header from "./components/Header";
 
 const NavFormBtnsLayout = () => {
   return (
@@ -28,19 +38,23 @@ export default function App() {
   }, [dispatch]);
 
   return (
-    <main>
-      <BrowserRouter>
-      <MainHeader/>
-        <Routes>
-          <Route path="/" element={<ListObjects />} />
-          <Route element={<NavFormBtnsLayout />}>
-            <Route path="/list-objects" element={<ListObjects />} />
-            <Route path="/formulaire-reservation" element={<Formulaire />} />
-          </Route>
-          <Route path='/reservation-confirmation/:response/:id' element={<Reservation/>}/>
-          <Route path='/mes-demarches' element={<Demarches/>}/>
-        </Routes>
+    <BrowserRouter>
+      <Header />
+      <main>
+          <MainHeader />
+          <Routes>
+            <Route path="/" element={<Navigate to="/list-objects" replace />} />
+            <Route element={<NavFormBtnsLayout />}>
+              <Route path="/list-objects" element={<ListObjects />} />
+              <Route path="/formulaire-reservation" element={<Formulaire />} />
+            </Route>
+            <Route
+              path="/reservation-confirmation/:response/:id"
+              element={<Reservation />}
+            />
+            <Route path="/mes-demarches" element={<Demarches />} />
+          </Routes>
+      </main>
       </BrowserRouter>
-    </main>
   );
 }
