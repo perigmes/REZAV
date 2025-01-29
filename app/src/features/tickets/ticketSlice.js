70f24cae8da1a4f3702a17a355356fbc3d7374e4
Pierrickbrd <Pierrick.Breaud.Etu@univ-lemans.fr>
2025-01-29 10:09:28 +0100

    idk

--- app/src/features/tickets/ticketSlice.js
+++ app/src/features/tickets/ticketSlice.js
@@ -1,8 +1,8 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { loadReservations } from './ticketAsyncAction.js';
+import { createSlice } from "@reduxjs/toolkit";
+import { loadReservations } from "./ticketAsyncAction.js";
 
 const ticketSlice = createSlice({
-  name: 'reservations',
+  name: "reservations",
   initialState: {
     reservations: [],
     selectedReservation: "",
@@ -11,8 +11,8 @@ const ticketSlice = createSlice({
       apiErrorLoad: null,
     },
     statusColors: {
-      "6032f523-fa8e-4226-ad7f-356d490446b6": "green",  // Approuvé
-      "317e64d4-6146-4f7e-a36c-016338899839": "red",    // Refusé
+      "6032f523-fa8e-4226-ad7f-356d490446b6": "green", // Approuvé
+      "317e64d4-6146-4f7e-a36c-016338899839": "red", // Refusé
       "96cfcafa-4580-4b55-88ca-6bb3d6a8e938": "orange", // En attente
     },
   },
@@ -24,22 +24,22 @@ const ticketSlice = createSlice({
       state.selectedReservation = action.payload;
     },
   },
-  extraReducers: (builder) =>{
+  extraReducers: (builder) => {
     builder
-            .addCase(loadReservations.pending, (state) => {
-                state.isLoading = true;
-                state.errors.apiErrorLoad = null;
-            })
-            .addCase(loadReservations.fulfilled, (state, action) => {
-                state.reservations = action.payload;
-                state.isLoading  = false;
-                state.errors.apiErrorLoad = null;
-            })
-            .addCase(loadReservations.rejected, (state, action) => {
-                state.isLoading = false;
-                state.errors.apiErrorLoad = action.payload;
-            })
-  }
+      .addCase(loadReservations.pending, (state) => {
+        state.isLoading = true;
+        state.errors.apiErrorLoad = null;
+      })
+      .addCase(loadReservations.fulfilled, (state, action) => {
+        state.reservations = action.payload;
+        state.isLoading = false;
+        state.errors.apiErrorLoad = null;
+      })
+      .addCase(loadReservations.rejected, (state, action) => {
+        state.isLoading = false;
+        state.errors.apiErrorLoad = action.payload;
+      });
+  },
 });
 
 export const { setReservations, selectReservation } = ticketSlice.actions;
