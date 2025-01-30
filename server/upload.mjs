dab43ddeeb0b5e7e755183a5c18a8bce4407a1de
perigmes <clemchallans@gmail.com>
2025-01-30 15:05:20 +0100

    test

--- server/upload.mjs
+++ server/upload.mjs
@@ -7,13 +7,7 @@ const storage = multer.diskStorage({
     cb(null, Date.now() + "-" + file.originalname); // Nom unique pour chaque fichier
   },
 });
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith("image/") || file.mimetype === "application/pdf") {
-    cb(null, true); // Accepter le fichier
-  } else {
-    cb(new Error("Seuls les fichiers images et PDF sont autorisés"), false);
-  }
-};
+
 
 // Configuration de `multer`
-export const upload = multer({ storage: storage, fileFilter: fileFilter });
+export const upload = multer({ storage: storage });
